1195dc6ee106e06fe76bb247966925cdc8afd8ca
Nathan Osman <nathan@quickmediasolutions.com>
2017-08-09 11:59:14 -0700

    Further improvements to the script.

--- betternode.sh
+++ betternode.sh
@@ -2,9 +2,12 @@
 
 USER=tmpuser
 
+# Remove the built-in node account
+userdel $(id -nu 1000`)
+
 # Create a new group and user with the correct values
 groupadd -g $GID $USER
-useradd -mu $UID $USER
+useradd -mu $UID -g $GID $USER
 
 # Switch to the specified user's account and run the command
 sudo -EHu $USER env "PATH=$PATH" "$@"
